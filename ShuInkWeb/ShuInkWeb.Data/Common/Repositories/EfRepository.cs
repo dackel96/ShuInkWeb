cbcdb1a40a7ac1cace08f5751d959fefda227a98
Ivan Iliev <ivanilievdackel@gmail.com>
2022-11-24 22:02:30 +0200

    update Repositories

--- ShuInkWeb/ShuInkWeb.Data/Common/Repositories/EfRepository.cs
+++ ShuInkWeb/ShuInkWeb.Data/Common/Repositories/EfRepository.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,9 +23,14 @@ namespace ShuInkWeb.Data.Common.Repositories
 
         public virtual IQueryable<TEntity> All() => this.DbSet;
 
-        public virtual IQueryable<TEntity> AllAsNoTracking() => this.DbSet.AsNoTracking();
+        public virtual IQueryable<TEntity> AllAsNoTracking()
+            => this.DbSet
+                   .AsNoTracking();
 
-        public virtual Task AddAsync(TEntity entity) => this.DbSet.AddAsync(entity).AsTask();
+        public virtual Task AddAsync(TEntity entity)
+            => this.DbSet
+                   .AddAsync(entity)
+                   .AsTask();
 
         public virtual void Update(TEntity entity)
         {
@@ -36,9 +43,13 @@ namespace ShuInkWeb.Data.Common.Repositories
             entry.State = EntityState.Modified;
         }
 
-        public virtual void Delete(TEntity entity) => this.DbSet.Remove(entity);
+        public virtual void Delete(TEntity entity)
+            => this.DbSet
+                   .Remove(entity);
 
-        public Task<int> SaveChangesAsync() => this.Context.SaveChangesAsync();
+        public Task<int> SaveChangesAsync()
+            => this.Context
+                   .SaveChangesAsync();
 
         public void Dispose()
         {
@@ -52,6 +63,54 @@ namespace ShuInkWeb.Data.Common.Repositories
             {
                 this.Context?.Dispose();
             }
+        }
+
+        public IQueryable<TEntity> All(Expression<Func<TEntity, bool>> search)
+            => this.DbSet
+                   .Where(search);
+
+        public IQueryable<TEntity> AllReadonly(Expression<Func<TEntity, bool>> search)
+            => this.DbSet
+                   .Where(search)
+                   .AsNoTracking();
+
+        public async Task<TEntity> GetByIdAsync(object id)
+            => (await DbSet.FindAsync(id))!;
+
+        public async Task<TEntity> GetByIdsAsync(object[] id)
+            => (await DbSet.FindAsync(id))!;
+
+        public async Task AddRangeAsync(IEnumerable<TEntity> entities)
+            => await this.DbSet
+                         .AddRangeAsync(entities);
+
+        public void UpdateRange(IEnumerable<TEntity> entities)
+            => this.DbSet
+                   .UpdateRange(entities);
+
+        public async Task DeleteAsync(object id)
+        {
+            TEntity entity = await GetByIdAsync(id);
+
+            Delete(entity);
+        }
+
+        public void DeleteRange(IEnumerable<TEntity> entities)
+        {
+            this.DbSet.RemoveRange(entities);
+        }
+
+        public void DeleteRange(Expression<Func<TEntity, bool>> deleteWhereClause)
+        {
+            var entities = All(deleteWhereClause);
+            DeleteRange(entities);
+        }
+
+        public void Detach(TEntity entity)
+        {
+            EntityEntry entry = this.Context.Entry(entity);
+
+            entry.State = EntityState.Detached;
         }
     }
 }
--- ShuInkWeb/ShuInkWeb.Data/Common/Repositories/IRepository.cs
+++ ShuInkWeb/ShuInkWeb.Data/Common/Repositories/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,13 +11,33 @@ namespace ShuInkWeb.Data.Common.Repositories
     {
         IQueryable<TEntity> All();
 
+        IQueryable<TEntity> All(Expression<Func<TEntity, bool>> search);
+
         IQueryable<TEntity> AllAsNoTracking();
+
+        IQueryable<TEntity> AllReadonly(Expression<Func<TEntity, bool>> search);
+
+        Task<TEntity> GetByIdAsync(object id);
+
+        Task<TEntity> GetByIdsAsync(object[] id);
 
         Task AddAsync(TEntity entity);
 
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+
         void Update(TEntity entity);
 
+        void UpdateRange(IEnumerable<TEntity> entities);
+
+        Task DeleteAsync(object id);
+
         void Delete(TEntity entity);
+
+        void DeleteRange(IEnumerable<TEntity> entities);
+
+        void DeleteRange(Expression<Func<TEntity, bool>> deleteWhereClause);
+
+        void Detach(TEntity entity);
 
         Task<int> SaveChangesAsync();
     }
