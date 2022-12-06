fa6d019c03edee0b1bce2b5856970f0180e22d9c
Ivan Iliev <ivanilievdackel@gmail.com>
2022-12-06 08:55:44 +0200

    Area for Shop part of the website

---
+++ ShuInkWeb/ShuInkWeb/Areas/Shop/Controllers/Home.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShuInkWeb.Areas.Shop.Controllers
+{
+    [Area("Shop")]
+    [Route("Shop/[controller]/[action]/{id?}")]
+    public class Home : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
